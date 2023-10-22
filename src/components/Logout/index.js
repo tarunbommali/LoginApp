4a2edd293e033eaee39248fb2e969fb58f79b86a
tarun <btaruntej143@gmail.com>
2023-10-22 13:29:11 +0000

    LoginApp

---
+++ src/components/Logout/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Logout = props => {
+  const {logout} = props
+  return (
+    <button type="button" className="logout-button" onClick={logout}>
+      Logout
+    </button>
+  )
+}
+
+export default Logout
